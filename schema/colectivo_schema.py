ee79bf0060e6e855c9ea392348361765f539cf57
Mayito vicente <cv2143304@gmail.com>
2024-11-22 13:18:24 -0600

    id,model-and-main

--- schema/colectivo_schema.py
+++ schema/colectivo_schema.py
@@ -2,9 +2,9 @@ from pydantic import BaseModel
 from typing import Optional
 
 class ColectivoSchema (BaseModel):
-    id: Optional[int]
+    id: Optional[int] = None
     asientos: int
     ubicacion: str
-    num_serie: int
+    num_serie: str
     fecha : str
     horario : str
