208343464e3792fa07de87f89438af703f7ecb0f
Angelgabrielruiz <aa09angel@gmail.com>
2024-11-22 13:23:32 -0600

    Guardando cambios locales en colectivo_schema.py

--- schema/colectivo_schema.py
+++ schema/colectivo_schema.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel
 from typing import Optional
 
 class ColectivoSchema (BaseModel):
-    id: Optional[int] = None
+    id: Optional[int]
     asientos: int
     ubicacion: str
     num_serie: int
